510e9651fce86e6f68aad319bfa9b4fe96390bfe
Thomas Lindeker <tlindeker@gmail.com>
2023-02-27 15:08:45 +0000

    z

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,10 @@ import IconButton from "@material-ui/core/IconButton"
 import TextField from "@material-ui/core/TextField"
 import AssignmentIcon from "@material-ui/icons/Assignment"
 import PhoneIcon from "@material-ui/icons/Phone"
+import VideocamIcon from '@material-ui/icons/Videocam';
+import VideocamOffIcon from '@material-ui/icons/VideocamOff';
+import MicIcon from '@material-ui/icons/Mic';
+import MicOffIcon from '@material-ui/icons/MicOff';
 import React, { useEffect, useRef, useState } from "react"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 import Peer from "simple-peer"
@@ -10,7 +14,8 @@ import io from "socket.io-client"
 import "./App.css"
 
 
-const socket = io.connect('http://localhost:5000')
+
+const socket = io.connect('https://api.impin.fr')
 function App() {
 	const [ me, setMe ] = useState("")
 	const [ stream, setStream ] = useState()
@@ -21,15 +26,42 @@ function App() {
 	const [ idToCall, setIdToCall ] = useState("")
 	const [ callEnded, setCallEnded] = useState(false)
 	const [ name, setName ] = useState("")
+	const [ mic, setmic ] = useState(true)
+	const [ video, setVideo ] = useState(true)
 	const myVideo = useRef()
 	const userVideo = useRef()
 	const connectionRef= useRef()
 
+
+	// new thread every 1 second we econsole.log the stream
+	
+	useEffect(() => {
+		if (stream) {
+		  stream.getVideoTracks()[0].enabled = video;
+		}
+
+		console.log(video)
+	}, [video]);
+
+	  useEffect(() => {
+		if (stream) {
+			stream.getAudioTracks()[0].enabled = mic;
+		}
+		console.log(mic)
+	}, [mic]);
+
+
+	
+
 	useEffect(() => {
 		navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
 			setStream(stream)
+			if (video) {
 				myVideo.current.srcObject = stream
+			}
 		})
+
+		
 
 	socket.on("me", (id) => {
 			setMe(id)
@@ -95,22 +127,31 @@ function App() {
 
 	return (
 		<>
-			<h1 style={{ textAlign: "center", color: '#fff' }}>Zoomish</h1>
+			<h1 style={{ textAlign: "center", color: '#fff' }}>Zoom de wish</h1>
 		<div className="container">
 			<div className="video-container">
 				<div className="video">
-					{stream &&  <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
+				{stream && (
+					<video
+						playsInline
+						ref={myVideo}
+						muted={mic}
+						autoPlay
+						style={{display: video ? "block" : "none"}}
+						className="myVideo"
+					/>
+					)}
 				</div>
 				<div className="video">
 					{callAccepted && !callEnded ?
-					<video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} />:
+					<video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} className="myVideo" /> :
 					null}
 				</div>
 			</div>
 			<div className="myId">
 				<TextField
 					id="filled-basic"
-					label="Name"
+					label="Pseudo"
 					variant="filled"
 					value={name}
 					onChange={(e) => setName(e.target.value)}
@@ -118,13 +159,13 @@ function App() {
 				/>
 				<CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
 					<Button variant="contained" color="primary" startIcon={<AssignmentIcon fontSize="large" />}>
-						Copy ID
+						Copier mon ID
 					</Button>
 				</CopyToClipboard>
 
 				<TextField
 					id="filled-basic"
-					label="ID to call"
+					label="ID à appeler"
 					variant="filled"
 					value={idToCall}
 					onChange={(e) => setIdToCall(e.target.value)}
@@ -141,11 +182,23 @@ function App() {
 					)}
 					{idToCall}
 				</div>
+				<div className="settings-button">
+					<IconButton color='primary'  onClick={() => {
+						setVideo(!video)
+						console.log(video)
+						}}>
+						{video ? <VideocamIcon fontSize="large" /> : <VideocamOffIcon fontSize="large" />}
+					</IconButton>
+					<IconButton color='primary' onClick={() => setmic(!mic)}>
+						{mic ? <MicIcon fontSize="large" /> : <MicOffIcon fontSize="large" />}
+					</IconButton>
+
+				</div>
 			</div>
 			<div>
 				{receivingCall && !callAccepted ? (
 						<div className="caller">
-						<h1 >{name} is calling...</h1>
+						<h1 >{name} est en train d'appeler...</h1>
 						<Button variant="contained" color="primary" onClick={answerCall}>
 							Answer
 						</Button>
